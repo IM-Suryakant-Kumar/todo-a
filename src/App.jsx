49b91196a6b30e8dc35602386a634e898d56a2bf
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2024-07-06 14:16:09 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Stack } from '@mui/material'
+
+function App() {
+
+  return (
+    <Stack component="main" sx={{  }}>
+      {/* <Header /> */}
+      {/* TaskInput */}
+      {/* TaskList */}
+    </Stack>
+  )
+}
+
+export default App
